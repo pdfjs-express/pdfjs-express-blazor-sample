c2097385b4d8d12cf107c94e4440986fb406991e
dustinriley <dustin.riley@me.com>
2020-10-16 10:51:51 -0700

    fixed incomplete copy of files

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -3,6 +3,8 @@ const fs = require('fs-extra');
 const copyFiles = async () => {
   try {
     await fs.copy('./node_modules/@pdftron/pdfjs-express/public', './wwwroot/lib');
+    await fs.copy('./node_modules/@pdftron/pdfjs-express/webviewer.min.js', './wwwroot/lib/webviewer.min.js');
+
     console.log('WebViewer files copied over successfully');
   } catch (err) {
     console.error(err);
